19e5fb8ad3df60ceb2af3657b15b9e7043a039e2
Alberto Bonacina <bonacina.alberto@gmail.com>
2017-06-04 18:36:54 +0200

    Remove useless comment

--- EobotAPI.kt
+++ EobotAPI.kt
@@ -5,9 +5,6 @@ import com.androidnetworking.common.Priority
 import com.androidnetworking.interfaces.JSONObjectRequestListener
 import java.util.logging.Logger
 
-/**
- * Created by alberto on 02/06/17.
- */
 class EobotAPI {
 
     companion object {
